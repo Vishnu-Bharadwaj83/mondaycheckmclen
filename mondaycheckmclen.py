d094e6bf29f6e6398a276f972872530bf0f8d6df
Vishnu-Bharadwaj83 <70822370+Vishnu-Bharadwaj83@users.noreply.github.com>
2020-10-04 15:16:30 -0700

    Add files via upload

---
+++ mondaycheckmclen.py
@@ -0,0 +1,17 @@
+# mondaycheckmclen.py
+
+schedule = input('Enter schedule here:')
+
+Monday = 'Monday'
+
+if Monday in schedule:
+    print('Monday is present.')
+
+else:
+    print('Monday is not present.')
+
+print('')
+
+print('Length of your schedule:')
+
+print(len(schedule))
